802680d4c5cf8a820b57ad154d4f2b968f004ae2
June1010 <seukjun1010@gmail.com>
2020-11-29 14:47:34 +0900

    add JSJ function.

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 void KSH(int a){
     printf("과제가 하고 싶다면 당근을 흔들어 주세요\n"); //과제로 변경
-    for (int i = 0; i < a; i++)
+    int i;
+	for (i = 0; i < a; i++)
     {
         printf("당근");
     }
     
 }
+
+void JSJ() {
+	char hw[2][10] = {"시프실", "남데베"};
+	int i;
+	for(i = 0; i < 2; i++)
+		printf("%s 과제 말고 과자 주세요\n", hw[i]);
+}
+
 
 int main(){
     printf("Hello OSS!\n");
@@ -15,8 +24,8 @@ int main(){
     
     //김상훈 commit, KSH 함수 생성
     //길은지 printf Team 9 추가
+    //정석준 JSJ 함수 생성
     int helpme = 3;
     KSH(helpme);
-
-
+    JSJ();
 }
