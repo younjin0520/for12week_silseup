4bb8fd2927a5c0ea1ea2e82a7545d6b3245aafad
EunJiGil <55980214+EunJiGil@users.noreply.github.com>
2020-11-29 13:10:34 +0900

    Update main.c
    Changed 종강 to 과제

--- main.c
+++ main.c
@@ -1,74 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-
-#include <time.h>
-
-long totalday(int y, int m, int d);
-
- 
+void KSH(int a){
+    printf("과제가 하고 싶다면 당근을 흔들어 주세요\n"); //과제로 변경
+    for (int i = 0; i < a; i++)
+    {
+        printf("당근");
+    }
+    
+}
 
 int main(){
-        printf("Hello OSS!\n");
-        int sy, sm, sd, fy, fm, fd, dates;
+    printf("Hello OSS!\n");
 
- 
+    //아무 코드나 추가해서 commit 해주세요..
+    
+    //김상훈 commit, KSH 함수 생성
+    int helpme = 3;
+    KSH(helpme);
 
-        printf("첫번째 년 월일 입력 \n");
-
-        printf("형식 1998 8 23  : ");
-
-        scanf("%d %d %d", &fy, &fm, &fd);
-
- 
-
-        printf("두번째 년 월일 입력 \n");
-
-        printf("형식 1998 8 23  : ");
-
-        scanf("%d %d %d", &sy, &sm, &sd);
-
- 
-
-        dates = totalday(sy, sm, sd) - totalday(fy, fm, fd);
-
-        printf("\n\n%d년 %d월 %d일 과 \n", fy, fm, fd);
-
-        printf("%d년 %d월 %d일 사이의 \n", sy, sm, sd);
-
-        printf("총 날짜 수 : %ld일\n", dates);
-
-        return 0;
 
 }
-
- 
-
-long totalday(int y, int m, int d){
-
-        int months[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
-
-        int i;
-
-        long total = 0L;
-
- 
-
-        total = (y - 1) * 365L + (y - 1) / 4 - (y - 1) / 100 + (y - 1) / 400;
-
-        if (!(y % 4) && y % 100 || !(y % 400))
-
-                 months[1]++;
-
-        for (i = 0; i < m - 1; i++)
-
-                 total += months[i];
-
-        total += d;
-
- 
-
-        return total;
-
-}
-   
